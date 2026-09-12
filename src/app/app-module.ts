cc18fae2d66a2e0396c2f21ba887a351ec9efb7c
Your Name <your.email@example.com>
2026-09-12 20:27:39 -0300

    Finalizando a interface

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './template/header/header';
+import { Footer } from './template/footer/footer';
+import { Home } from './components/home/home';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Header, Footer, Home],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
